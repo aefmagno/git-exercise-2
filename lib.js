8b7a70da9943c3f3c05b9004256b3e01842c9993
Aaron Magno <77775186+aefmagno@users.noreply.github.com>
2022-10-05 09:32:47 +0800

    added  new code implementation module-part-2 with all required functions (#1)
    function implementation

---
+++ lib.js
@@ -0,0 +1,15 @@
+function square(n) {
+    return n * n;
+}
+
+function fahrenheit(celsius) {
+    return (celsius * (9 / 5)) + 32;
+}
+
+function rectangleArea(a, b) {
+    return a * b;
+}
+
+function isPalindrome(str) {
+    return str.toLowerCase() === str.toLowerCase().split('').reverse().join('');
+}

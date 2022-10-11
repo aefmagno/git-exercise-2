555a8516eea110fb3456258935f7e3427fcc6032
Aaron Magno <aefmagno@gmaik.com>
2022-10-11 09:14:01 +0800

    applied prettier linter

--- lib.js
+++ lib.js
@@ -1,22 +1,22 @@
 function square(n) {
-    return n * n;
+  return n * n;
 }
 
 function fahrenheit(celsius) {
-    return (celsius * (9 / 5)) + 32;
+  return celsius * (9 / 5) + 32;
 }
 
 function rectangleArea(a, b) {
-    return a * b;
+  return a * b;
 }
 
 function isPalindrome(str) {
-    return str.toLowerCase() === str.toLowerCase().split('').reverse().join('');
+  return str.toLowerCase() === str.toLowerCase().split('').reverse().join('');
 }
 
 module.exports = {
-    square,
-    fahrenheit,
-    rectangleArea,
-    isPalindrome
-}
+  square,
+  fahrenheit,
+  rectangleArea,
+  isPalindrome,
+};
